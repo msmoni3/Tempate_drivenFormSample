817efb048696ab522ab35b3899fc64cb58764977
msmoni3 <msmoni3@gmail.com>
2020-09-06 20:30:47 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Sample of Template Driven Form' + VERSION.major;
+
+  register(registrationForm){
+    console.log(registrationForm.value,"registered")
+  }
+}
